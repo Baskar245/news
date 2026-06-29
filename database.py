4b4ad47ee4d4d293bb95dd12220ac1f3af708fa8
sqlpayloads <newtonbaskar04@gmail.com>
2026-06-29 22:46:20 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 import psycopg2
 
-conn = psycopg2.connect(
-    host="ep-cold-shape-apri4rwd.c-7.us-east-1.aws.neon.tech",
-    database="neondb",
-    user="neondb_owner",
-    password="npg_EMTo4ubGdWB8",
-    port="5432",
-    sslmode="require"
-)
 
-cursor = conn.cursor()
+def get_connection():
+    return psycopg2.connect(
+        host="ep-cold-shape-apri4rwd.c-7.us-east-1.aws.neon.tech",
+        database="neondb",
+        user="neondb_owner",
+        password="npg_EMTo4ubGdWB8",
+        port="5432",
+        sslmode="require"
+    )
